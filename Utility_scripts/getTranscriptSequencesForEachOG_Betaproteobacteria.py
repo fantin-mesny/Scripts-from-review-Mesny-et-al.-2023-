6130bbda1f3bed9f21f2bbe8eea89f35b10a1a45
Fantin Mesny <30922104+fantin-mesny@users.noreply.github.com>
2023-06-21 17:13:49 +0200

    fixed errors in paths in python scripts

--- Utility_scripts/getTranscriptSequencesForEachOG_Betaproteobacteria.py
+++ Utility_scripts/getTranscriptSequencesForEachOG_Betaproteobacteria.py
@@ -1,15 +1,16 @@
 from Bio import SeqIO
 import os
+scriptDir=os.path.dirname(os.path.realpath(__file__))
 
-with open('Genomic_data/Betaproteobacteria/cds/all.ffn','r') as inp:
+with open(scriptDir+'/../Genomic_data/Betaproteobacteria/cds/all.ffn','r') as inp:
 	allt=SeqIO.to_dict(SeqIO.parse(inp, 'fasta'))
 
-Dir='Genomic_data/Betaproteobacteria/OrthoFinder/Results/Orthogroup_Sequences/'
+Dir=scriptDir+'/../Genomic_data/Betaproteobacteria/OrthoFinder/Results/Orthogroup_Sequences/'
 for f in os.listdir(Dir):
 	tseqs=[]
 	with open(Dir+f, 'r') as inp:
 		seqs=SeqIO.to_dict(SeqIO.parse(inp,'fasta'))
 	for seq in seqs:
 		tseqs.append(allt[seq])
-	with open('Genomic_data/Betaproteobacteria/OrthoFinder/Results/Orthogroup_Sequences_Transcripts/'+f.replace('fa','ffn'),'w+') as outp:
+	with open(scriptDir+'/../Genomic_data/Betaproteobacteria/OrthoFinder/Results/Orthogroup_Sequences_Transcripts/'+f.replace('fa','ffn'),'w+') as outp:
 		SeqIO.write(tseqs,outp,'fasta')
